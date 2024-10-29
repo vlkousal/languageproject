f6269f053f26272fb6684d8b2adc04832af1214e
Vladimír Kousal <vladimirkousal@protonmail.com>
2024-10-29 12:55:31 +0100

    rewrote draw mode checking

--- front/src/app/game/draw-characters/draw-characters.component.ts
+++ front/src/app/game/draw-characters/draw-characters.component.ts
@@ -13,8 +13,13 @@ import {CookieService} from "ngx-cookie";
 export class DrawCharactersComponent extends GameComponent {
 
     @Output() onGoBack: EventEmitter<void> = new EventEmitter();
-    correctMinimum: number = 0;
     allowAnswering: boolean = true;
+
+    BACKGROUND_COLOR: string = "#F9F8EB";
+    HINT_COLOR: string = "#818589";
+    PEN_COLOR: string = "#000000";
+
+    ACCEPTABLE_PERCENTAGE: number = 82.5;
 
     constructor(cookieService: CookieService) {
         super(Mode.DrawCharacters, cookieService);
@@ -30,42 +35,17 @@ export class DrawCharactersComponent extends GameComponent {
         context.clearRect(0, 0, canvas.width, canvas.height);
         testingContext.clearRect(0, 0, canvas.width, canvas.height);
 
-        context.fillStyle = "#F9F8EB";
+        context.fillStyle = this.BACKGROUND_COLOR;
         context.fillRect(0, 0, canvas.width, canvas.height);
 
-        testingContext.fillStyle = "#F9F8EB";
+        testingContext.fillStyle = this.BACKGROUND_COLOR;
         testingContext.fillRect(0, 0, canvas.width, canvas.height);
 
-        context.fillStyle = "grey"
+        context.fillStyle = this.HINT_COLOR;
         context.fillText(this.words[this.index].question, 0, 170);
 
-        testingContext.fillStyle = "black";
+        testingContext.fillStyle = this.PEN_COLOR;
         testingContext.fillText(this.words[this.index].question, 0, 170);
-
-        this.correctMinimum = 40000 - ((40000 - this.getBlackPixelCount()) / 2) - 2000;
-    }
-
-    getBlackPixelCount(): number {
-        const testingCanvas = document.getElementById("testingCanvas") as HTMLCanvasElement;
-        const testingContext = testingCanvas.getContext('2d');
-        if(testingContext == null) return -1;
-        const width: number = testingCanvas.width;
-        const height: number = testingCanvas.height;
-        const imageData = testingContext.getImageData(0, 0, 200, 200).data;
-
-        let counter: number = 0;
-        for (let y = 0; y < height; y++) {
-            for (let x = 0; x < width; x++) {
-                const index = (y * width + x) * 4;
-                const r = imageData[index];
-                const g = imageData[index + 1];
-                const b = imageData[index + 2];
-                const a = imageData[index + 3];
-
-                if(this.rgbToHex(r, g, b, a) == "#f9f8ebfe01") counter++;
-            }
-        }
-        return counter;
     }
 
     override replayAll(): void {
@@ -95,11 +75,47 @@ export class DrawCharactersComponent extends GameComponent {
         const testingCanvas = document.getElementById("testingCanvas") as HTMLCanvasElement;
         const canvasContext = canvas.getContext('2d');
         const testingCanvasContext = testingCanvas.getContext('2d');
-        if(canvasContext == null) return;
-        if(testingCanvasContext != null) {
-            testingCanvasContext.fillText(currentWord.question, 0, 170);
+        if(canvasContext == null || testingCanvasContext == null) return;
+
+        testingCanvasContext.fillText(currentWord.question, 0, 170);
+
+
+        const width: number = testingCanvas.width;
+        const height: number = testingCanvas.height;
+        const canvasData = canvasContext.getImageData(0, 0, width, height).data;
+        const testData = testingCanvasContext.getImageData(0, 0, width, height).data;
+        let counter: number = 0;
+        for (let y = 0; y < height; y++) {
+            for (let x = 0; x < width; x++) {
+                const index = (y * width + x) * 4;
+
+                const r1 = canvasData[index];
+                const r2 = testData[index];
+
+                const g1 = canvasData[index];
+                const g2 = testData[index];
+
+                const b1 = canvasData[index];
+                const b2 = testData[index];
+
+                const canvasColor: string = this.rgbToHex(r1, g1, b1);
+                const testCanvasColor: string = this.rgbToHex(r2, g2, b2);
+
+                // checks whether the whole canvas isn't filled
+
+                if(canvasColor == testCanvasColor) {
+                    counter++;
+                }
+
+                if(testCanvasColor == this.PEN_COLOR && canvasColor != this.PEN_COLOR) {
+                    counter--;
+                }
+            }
         }
-        const isCorrect: boolean = this.getCorrectPixelCount() > this.correctMinimum;
+        const percentage: number = (counter / (200 * 200)) * 100;
+        console.log(counter + " / " + (200 * 200) + " - " + percentage + "%");
+
+        const isCorrect: boolean = percentage >= this.ACCEPTABLE_PERCENTAGE;
 
         canvasContext.fillStyle = "#59bd59";
         this.sendResult(isCorrect);
@@ -125,48 +141,14 @@ export class DrawCharactersComponent extends GameComponent {
                 }
                 return;
             }
-
         }, 1500);
     }
 
-    getCorrectPixelCount(): number {
-        const canvas = document.getElementById("canvas") as HTMLCanvasElement;
-        const context = canvas.getContext('2d');
-        const testingCanvas = document.getElementById("testingCanvas") as HTMLCanvasElement;
-        const testingContext = testingCanvas.getContext('2d');
-
-        if(context == null || testingContext == null) return -1;
-        const width = canvas.width;
-        const height = canvas.height;
-
-        const imageData = context.getImageData(0, 0, 200, 200).data;
-        const testingImageData = testingContext.getImageData(0, 0, 200, 200).data;
-        let correctPixels: number = 0;
-
-        for (let y = 0; y < height; y++) {
-            for (let x = 0; x < width; x++) {
-                const index = (y * width + x) * 4;
-                const r = imageData[index];
-                const g = imageData[index + 1];
-                const b = imageData[index + 2];
-                const a = imageData[index + 3] / 255;
-
-                const tR = testingImageData[index];
-                const tG = testingImageData[index + 1];
-                const tB = testingImageData[index + 2];
-                const tA = testingImageData[index + 3] / 255;
-                if(this.rgbToHex(r, g, b, a) == this.rgbToHex(tR, tG, tB, tA)) correctPixels++;
-            }
-        }
-        return correctPixels;
-    }
-
-    rgbToHex(r: number, g: number, b: number, a: number = 1): string {
+    rgbToHex(r: number, g: number, b: number): string {
         const red = r.toString(16).padStart(2, "0");
         const green = g.toString(16).padStart(2, "0");
         const blue = b.toString(16).padStart(2, "0");
-        const alpha = Math.round(a * 255).toString(16).padStart(2, '0');
-        return `#${red}${green}${blue}${alpha}`;
+        return `#${red}${green}${blue}`;
     }
 
 protected readonly SpeechUtils = SpeechUtils;
